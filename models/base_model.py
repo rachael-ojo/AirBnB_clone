26f404f0f660fdec955d1e69800d1e49e8dcf2d6
mudimurtala <ibnmuhyideen95@gmail.com>
2024-03-07 20:58:14 +0100

    Add methods to base_model.py file

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 
 import uuid
-import datetime
+from datetime import datetime
 
-class Basemodel:
+class BaseModel:
 
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+
+    
+    def __str__(self):
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+
+
+    
     def save(self):
-        pass
+        self.updated_at = datetime.now()
 
-    def to_dic(self):
-        pass
-
-    def self.__dict__(self):
-        pass
-
-    def __class__(self):
-        pass
+    def to_dict(self):
+        
