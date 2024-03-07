e34373fab55d4e3f925eeb43da02dcb52dffbf91
mudimurtala <ibnmuhyideen95@gmail.com>
2024-03-07 18:20:06 +0100

    Add BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import uuid
+import datetime
+
+class Basemodel:
+
+    def save(self):
+        pass
+
+    def to_dic(self):
+        pass
+
+    def self.__dict__(self):
+        pass
+
+    def __class__(self):
+        pass
