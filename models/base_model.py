4c6cc68a50c7f421724d7187c4497564ae36375a
mudimurtala <ibnmuhyideen95@gmail.com>
2024-03-08 08:22:43 +0100

    Review the functionality of **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
         if kwargs:
             for ky, val in kwargs.items():
                 if ky == 'created_at' or ky == 'updated_at':
-                    val = datetime.strptime(val, "%Y-%m-%dT%H:%M%S.%f")
+                    val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
                 if ky != '__class__':
                     setattr(self, ky, val)
         
@@ -34,3 +34,36 @@ class BaseModel:
         dict_ionary["created_at"] = dict_ionary["created_at"].isoformat()
         dict_ionary["updated_at"] = dict_ionary["updated_at"].isoformat()
         return dict_ionary
+    
+
+mudis_model = BaseModel()
+mudis_model.name = "Mudi"
+mudis_model.id_number = 150
+mudis_model.school = "Alx SE"
+
+dict_mudis_model = mudis_model.to_dict()
+
+print(dict_mudis_model)
+
+print("\n")
+
+# creating a new BaseModel instance from the dictionary
+
+rachael_model = BaseModel(**dict_mudis_model)
+
+print("Original model: ", mudis_model.name, mudis_model.school)
+print("\n")
+print("New model: ", rachael_model.name, rachael_model.school)
+
+print("\n")
+
+print("Original model: ", mudis_model)
+print("\n")
+print("New model: ", rachael_model)
+
+print("\n")
+
+print("Type of original created_at: ", type(mudis_model.created_at))
+print("Type of new created_at: ", type(rachael_model.created_at))
+
+
