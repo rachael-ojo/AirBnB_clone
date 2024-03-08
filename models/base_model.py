82bc61e6d6b1316ed1c167dba2177722d06a8a67
mudimurtala <ibnmuhyideen95@gmail.com>
2024-03-08 07:49:04 +0100

    Implement arguments handling

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,18 @@ from datetime import datetime
 
 class BaseModel:
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        
+        if kwargs:
+            for ky, val in kwargs.items():
+                if ky == 'created_at' or ky == 'updated_at':
+                    val = datetime.strptime(val, "%Y-%m-%dT%H:%M%S.%f")
+                if ky != '__class__':
+                    setattr(self, ky, val)
+        
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
 
     
