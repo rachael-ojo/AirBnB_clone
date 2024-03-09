3522847cd4c32c7222157ec8d736d1e699294d6b
mudimurtala <ibnmuhyideen95@gmail.com>
2024-03-09 07:14:35 +0100

    Update reload() method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 
 import json
+from models.base_model import BaseModel
+
+class_registry = {
+    'BaseModel': BaseModel
+}
+
 class FileStorage:
 
-    def __init__(self):
-        self.__file_path = ''
+    def __init__(self, file_path):
+        self.__file_path = file_path
         self.__objects = {}
 
     def all(self):
@@ -17,9 +23,18 @@ class FileStorage:
 
     def save(self):
         obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w') as f:
-            json.dump(obj_dict, f)
+        with open(self.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
 
 
     def reload(self):
-        pass
+        try:
+            with open(self.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+            for key, obj_data in obj_dict.items():
+                class_name = key.split(".")[0]
+                cla_s = class_registry.get(class_name)
+                obj = cla_s(**obj_data)
+                self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
