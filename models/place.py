9347ad086e55699e9de76f41fc458d7c685d9edf
mudimurtala <ibnmuhyideen95@gmail.com>
2024-03-10 17:23:55 +0100

    Add place.py file

---
+++ models/place.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""  """
+
+from models.base_model import BaseModel
+
+class Place(BaseModel):
+    """  """
+
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+    number_rooms = 0
+    name = ""
+    max_guest = 0
+    city_id = ""
+    number_bathrooms = 0
+    user_id = ""
+    description = ""
