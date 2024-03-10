3a5289db26cd13d03da013016ac844bb889ee0d5
mudimurtala <ibnmuhyideen95@gmail.com>
2024-03-10 17:32:19 +0100

    Add uesr.py file

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  """
+
+from models.base_model import BaseModel
+
+
+
+class User(BaseModel):
+    """  """
+
+    first_name = ""
+    last_name = ""
+    email = ""
+    password = ""
+    
