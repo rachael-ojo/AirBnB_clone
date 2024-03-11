e5a59029713b7381cc39af1d38429cb0648ff21e
mudimurtala <ibnmuhyideen95@gmail.com>
2024-03-11 10:03:13 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,5 @@
 
-<<<<<<< HEAD
-=======
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage('file_path')
 # storage.reload()
->>>>>>> d2d4f9dae6204c2f9234767e5470ac4283152bf6
